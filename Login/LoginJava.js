8fe658e3a00e81faffb2a680bb4421e9e3368b69
Nathan <axfonath14@live.trinity.sa.edu.au>
2018-09-27 14:13:01 +0930

    started the login just need to add feedback on whether it was succefull or not

---
+++ Login/LoginJava.js
@@ -0,0 +1,28 @@
+
+document.getElementById("WindowsButton").onclick = login();
+function login()
+{
+	autoLogIn("user13@test.com", "password");
+}
+
+
+function autoLogIn(un, pw) {
+	var form = document.createElement("form");
+	var element1 = document.createElement("input");
+	var element2 = document.createElement("input");
+
+	form.method = "POST";
+	form.action = "http://axfonath.000webhostapp.com/action_loginwebpage.php";
+
+	element1.value = un;
+	element1.name = "email";
+	form.appendChild(element1);
+
+	element2.value = pw;
+	element2.name = "password";
+	form.appendChild(element2);
+
+	document.body.appendChild(form);
+
+	form.submit();
+}
